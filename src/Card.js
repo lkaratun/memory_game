0d49db2a123af13da11a25279943f6439a0c9160
lkaratun <lev.karatun@gmail.com>
2018-10-28 01:16:40 -0400

    Added a pattern to unopened cards

--- src/Card.js
+++ src/Card.js
@@ -3,11 +3,14 @@ import './Card.css';
 
 const Card = ({color, opened, handleClick, cardSize}) => {
 	let classList = "card " + cardSize;
-	let newColor = opened ? color : "rgba(175, 175, 175, 0.42)";
+	// let newColor = opened ? color : "rgba(175, 175, 175, 0.42)";
+	const style = {}
+	if (opened) {style.backgroundColor = color}
+	else {classList = classList + " closed"};
   	return  	(
   		<div
   	className={classList}
-  	style={{backgroundColor: newColor}}
+  	style={style}
   	onClick={handleClick}
   	opened={opened.toString()}
   	>
