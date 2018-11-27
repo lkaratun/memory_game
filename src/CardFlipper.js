85f3d242cf5c0e058c2e749f85c1b8cb76327b43
lkaratun <lev.karatun@gmail.com>
2018-11-27 00:20:28 -0500

    Fix mobile sizing

--- src/CardFlipper.js
+++ src/CardFlipper.js
@@ -5,11 +5,15 @@ import Card from "./Card";
 class CardFlipper extends Component {
   render() {
     const { color, opened, handleClick, cardSize } = this.props;
+    let className = cardSize;
+    // console.log(classList);
     return (
-      <ReactCardFlip isFlipped={opened}>
-        <Card key="front" color={color} opened={false} cardSize={cardSize} handleClick={handleClick} />
-        <Card key="back" color={color} opened={true} cardSize={cardSize} handleClick={handleClick} />
-      </ReactCardFlip>
+      <div className={className}>
+        <ReactCardFlip isFlipped={opened} >
+          <Card key="front" color={color} opened={false} cardSize={cardSize} handleClick={handleClick} />
+          <Card key="back" color={color} opened={true} cardSize={cardSize} handleClick={handleClick} />
+        </ReactCardFlip>
+      </div>
       // <Card color={color} opened={opened} cardSize={cardSize} />
     );
   }
