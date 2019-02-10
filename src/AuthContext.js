471db556e29b355ff9dff2806180639102b4f1ec
lkaratun <lev.karatun@gmail.com>
2019-02-10 14:23:44 -0500

    WIP

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -3,14 +3,13 @@ import React, { Component } from "react";
 export const AuthContext = React.createContext();
 
 export class ContextProvider extends Component {
+  state = { turnsTaken: 0, logIn: this.logIn, logOut: this.logOut, loggedIn: false };
   logIn = data => {
-    // console.log(data);
     this.setState({ loggedIn: true, firstName: data.profileObj.givenName, lastName: data.profileObj.familyName });
   };
   logOut = () => {
     this.setState({ loggedIn: false });
   };
-  state = { turnsTaken: 0, logIn: this.logIn, logOut: this.logOut, loggedIn: false };
 
   render() {
     return <AuthContext.Provider value={this.state}>{this.props.children}</AuthContext.Provider>;
